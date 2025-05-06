76716c2ab16628eab7033fa69801bcca1514f90b
Ian Young <ianyoung2820@gmail.com>
2025-05-05 20:18:41 -0400

    W03 Prepare

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Test Job class
+        var job1 = new Job();
+        job1._company   = "Microsoft";
+        job1._jobTitle  = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear   = 2022;
+
+        var job2 = new Job();
+        job2._company   = "Apple";
+        job2._jobTitle  = "Manager";
+        job2._startYear = 2022;
+        job2._endYear   = 2023;
+
+        // Test Resume class
+        var myResume = new Resume();
+        myResume._name = "Allison Rose";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Display everything
+        myResume.Display();
     }
 }
